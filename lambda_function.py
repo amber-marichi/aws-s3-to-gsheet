045c983db13bb580fafd02fc783f4c5c31bdc39e
Max Bilokur <bilmaxim@gmail.com>
2023-08-10 16:01:06 +0300

    docker file updated

--- lambda_function.py
+++ lambda_function.py
@@ -9,16 +9,20 @@ def lambda_handler(event, context):
         aws_access_key_id=os.environ.get("AWS_KEY_ID"),
         aws_secret_access_key=os.environ.get("AWS_ACCESS_KEY"),
     )
-    s3_client = my_session.client("s3")
+
+    csv_data = read_file_from_s3(my_session)
+    write_sheet(csv_data)
+
+
+def read_file_from_s3(session):
+    s3_client = session.client("s3")
     s3_response = s3_client.get_object(
         Bucket=os.environ.get("BUCKET_NAME"),
         Key=os.environ.get("FILE_NAME")
     )
     
     file_data = s3_response["Body"].read().decode("UTF-8").splitlines()
-    csv_data = csv.reader(file_data)
-
-    write_sheet(csv_data)
+    return csv.reader(file_data)
 
 
 def write_sheet(name_age_list):
