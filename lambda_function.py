8ecbf7f5b575a36516e522a86243c93dbad11eb0
Max Bilokur <bilmaxim@gmail.com>
2023-08-14 11:27:12 +0300

    removed session to test lambda from cr

--- lambda_function.py
+++ lambda_function.py
@@ -7,22 +7,17 @@ from googleapiclient.discovery import build
 
 
 def lambda_handler(event, context):
-    my_session = boto3.Session(
-        aws_access_key_id=os.environ.get("AWS_KEY_ID"),
-        aws_secret_access_key=os.environ.get("AWS_ACCESS_KEY"),
-    )
-
-    csv_data = read_file_from_s3(my_session)
+    csv_data = read_file_from_s3()
     write_sheet(csv_data)
 
 
-def read_file_from_s3(session):
-    s3_client = session.client("s3")
+def read_file_from_s3():
+    s3_client = boto3.client("s3")
     s3_response = s3_client.get_object(
         Bucket=os.environ.get("BUCKET_NAME"),
         Key=os.environ.get("FILE_NAME")
     )
-    
+
     file_data = s3_response["Body"].read().decode("UTF-8").splitlines()
     return csv.reader(file_data)
 
