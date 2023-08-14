65f5bb51bc3cd140d87a25e745a54f26cdd134a4
Max Bilokur <bilmaxim@gmail.com>
2023-08-14 15:23:25 +0300

    yml env refactor

--- lambda_function.py
+++ lambda_function.py
@@ -33,7 +33,7 @@ def get_gsheet():
 def write_sheet(name_age_list):
     gsheet = get_gsheet()
     rows = {
-        'values' : list(name_age_list)[1:4]
+        'values' : list(name_age_list)[1:]
     }
 
     resp = gsheet.spreadsheets().values().append(
