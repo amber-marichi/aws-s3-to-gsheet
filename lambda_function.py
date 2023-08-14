9c26dae50bc2d59602c8eb91d37fcaa6506e6992
Max Bilokur <bilmaxim@gmail.com>
2023-08-14 21:48:50 +0300

    refactor for lambda secrets

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,51 @@
 import boto3
 import csv
-import os
+import json
+from botocore.exceptions import ClientError
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 
+def get_secret(secret_name):
+    region_name = "eu-central-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name="secretsmanager",
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as error:
+        raise error
+
+    # Decrypts secret using the associated KMS key.
+    return json.loads(get_secret_value_response["SecretString"])
+
+
 def lambda_handler(event, context):
-    csv_data = read_file_from_s3()
-    write_sheet(csv_data)
+    secrets = get_secret("vars-for-etl")
+
+    csv_data = read_file_from_s3(
+        secrets["bucket_name"],
+        secrets["file_name"]
+    )
+
+    write_sheet(
+        secrets["spreadsheet_id"],
+        csv_data
+    )
 
 
-def read_file_from_s3():
+def read_file_from_s3(bucket_name, file_name):
     s3_client = boto3.client("s3")
     s3_response = s3_client.get_object(
-        Bucket=os.environ.get("BUCKET_NAME"),
-        Key=os.environ.get("FILE_NAME")
+        Bucket=bucket_name,
+        Key=file_name
     )
 
     file_data = s3_response["Body"].read().decode("UTF-8").splitlines()
@@ -23,23 +53,24 @@ def read_file_from_s3():
 
 
 def get_gsheet():
-    credentials = service_account.Credentials.from_service_account_file(
-        "google_service_account_creds.json",
+    credentials = service_account.Credentials.from_service_account_info(
+        get_secret("google-service-acc-creds"),
         scopes=["https://www.googleapis.com/auth/spreadsheets"]
     )
     return build("sheets", "v4", credentials=credentials)
 
 
-def write_sheet(name_age_list):
+def write_sheet(spreadsheet_id, name_age_list):
     gsheet = get_gsheet()
     rows = {
-        'values' : list(name_age_list)[1:]
+        "values" : list(name_age_list)[1:]
     }
 
     resp = gsheet.spreadsheets().values().append(
-        spreadsheetId=os.environ.get("SPREADSHEET_ID"),
+        spreadsheetId=spreadsheet_id,
         range="A1",
         valueInputOption="RAW",
-        body=rows).execute()
+        body=rows
+    ).execute()
 
     print(f"{resp['updates']['updatedRows']} new rows added.")
